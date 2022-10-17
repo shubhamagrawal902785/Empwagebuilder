67f10c5439aa2c3ad5cda4a928b05440a4436c34
shubhamagrawal902785 <shubhamag937@gmail.com>
2022-10-17 23:59:22 +0530

    Welcome to employee wage

---
+++ src/java_bridgelabz_Day3/Empwage.java
@@ -0,0 +1,22 @@
+package java_bridgelabz_Day3;
+
+public class Empwage {
+	
+	public static void main(String[] args)
+	{
+		     System.out.println("Welcome to employee wage");
+		int a = (int)(Math.random() * 2);
+		  if(a==1)
+		  {
+			  System.out.println("Employee is present");
+		  }
+		  else
+		  {
+			  System.out.println("Employee is present");  
+		  }
+		  
+				
+				
+	}
+
+}
